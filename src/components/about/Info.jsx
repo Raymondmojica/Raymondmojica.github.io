2e241730d6e66ed8ef217c7001dc6993610c59c8
Raymond Mojica <rm@M12020.home>
2022-09-27 14:47:46 -0400

    Styled About Me section

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -4,18 +4,21 @@ const Info = () => {
   return (
     <div className="about__info grid">
       <div className="about__box">
+        <i class="bx bx-award about__icon"></i>
         <h3 className="about__title">Experience</h3>
         <span className="about__subtitle">1 year</span>
       </div>
 
       <div className="about__box">
+        <i class="bx bx-briefcase-alt about__icon"></i>
         <h3 className="about__title">Projects</h3>
         <span className="about__subtitle">MERN, CRUD, </span>
       </div>
 
       <div className="about__box">
-        <h3 className="about__title"></h3>
-        <span className="about__subtitle"></span>
+        <i class="bx bx-support about__icon"></i>
+        <h3 className="about__title">Support</h3>
+        <span className="about__subtitle">Online 24/7</span>
       </div>
     </div>
   );
