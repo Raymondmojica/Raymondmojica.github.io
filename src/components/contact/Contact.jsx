5b77a35f00c1e5e016480c3a53487721c845ce5e
Raymond Mojica <rm@M12020.home>
2022-10-19 16:04:33 -0400

    Personalizing

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -14,8 +14,10 @@ const Contact = () => {
             <div className="contact__card">
               <i className="bx bx contact__card-icon"></i>
 
-              <h3 className="contact__card-title"></h3>
-              <span className="contact__card-data"></span>
+              <h3 className="contact__card-title">Email</h3>
+              <span className="contact__card-data">
+                Raymondmojicajr@gmail.com
+              </span>
 
               <a href="" className="contact__button">
                 Write Me{" "}
